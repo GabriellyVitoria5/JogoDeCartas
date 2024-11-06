6683ae150d118d645d752316b896c4556c13f7c2
GabOof <oliveirafonseca.gab@gmail.com>
2024-11-06 19:05:02 -0300

    Torna todos os tipos publicos e adiciona verificação de baralho

--- src/main/kotlin/Carta.kt
+++ src/main/kotlin/Carta.kt
@@ -1,13 +1,15 @@
 // Classe que representa uma carta em um jogo de cartas colecionáveis
 class Carta(
     val nome: String,                // Nome da carta
-    private val descricao: String,   // Descrição da carta
+    val descricao: String,   // Descrição da carta
     val ataque: Int,                 // Valor de ataque
     val defesa: Int,                 // Valor de defesa
-    private val tipo: String         // Tipo da carta
+    val tipo: String         // Tipo da carta
 ) {
     // Estado inicial do monstro: pode ser "ataque" ou "defesa"
     var estado: String = "ataque"
+
+    var posicionada: Boolean = false // Indica se a carta está em campo
 
     // Retorna uma representação em string da carta
     override fun toString(): String {
